c056e4d65283a8d34bc9e37a244d4881cbc0601c
Caroline <caroline_mendez@baylor.edu>
2020-07-05 16:36:12 -0500

    Snake game

--- snake.py
+++ snake.py
@@ -19,7 +19,7 @@ head.speed(0)
 head.shape("square")
 head.color("lightblue")
 head.penup()
-head.goto(0, 0)
+head.goto(100, 100)
 head.direction = "stop"
 
 
@@ -39,6 +39,7 @@ def move():
         head.setx(x - 10)
 
 
+# Set movement
 def move_up():
     head.direction = "up"
 
@@ -55,13 +56,27 @@ def move_right():
     head.direction = "right"
 
 
+# Adding food
+food = turtle.Turtle()
+food.speed()
+food.shape("circle")
+food.color("blue")
+food.penup()
+food.shapesize(.5, .5)
+food.goto(0, 0)
+
+# Move food randomly
+if head.distance(food) < 10:
+    x = random.randint(-290, 290)
+    y = random.randint(-290, 290)
+    food.goto(x, y)
+
 # Keyboard binding, move around
 screen.listen()
 screen.onkeypress(move_up, "w")
 screen.onkeypress(move_down, "s")
 screen.onkeypress(move_left, "a")
 screen.onkeypress(move_right, "d")
-
 
 # Main loop
 while True:
