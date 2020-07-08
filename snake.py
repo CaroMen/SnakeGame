012faf53bcbfe6d6c2336726e321314412b3716f
Caroline <caroline_mendez@baylor.edu>
2020-07-08 00:52:45 -0500

    Snake Game

--- snake.py
+++ snake.py
@@ -41,19 +41,23 @@ def move():
 
 # Set movement
 def move_up():
-    head.direction = "up"
+    if head.direction != "down":
+        head.direction = "up"
 
 
 def move_down():
-    head.direction = "down"
+    if head.direction != "up":
+        head.direction = "down"
 
 
 def move_left():
-    head.direction = "left"
+    if head.direction != "right":
+        head.direction = "left"
 
 
 def move_right():
-    head.direction = "right"
+    if head.direction != "left":
+        head.direction = "right"
 
 
 # Adding food
@@ -74,32 +78,63 @@ screen.onkeypress(move_right, "d")
 
 seg = []
 
-# add tail
-new_seg = turtle.Turtle()
-new_seg.speed(0)
-new_seg.shape("square")
-new_seg.color("white")
-new_seg.penup()
-seg.append(new_seg)
-
-# move the tail
-for index in range(len(seg) - 1, 0, -1):
-    x = seg[index - 1].xcor()
-    y = seg[index - 1].ycor()
-    seg[index].goto(x, y)
-
-
 # Main loop
 while True:
     screen.update()
 
     # Move food randomly
-    if head.distance(food) < 30:
+    if head.distance(food) < 20:
         x = random.randint(-290, 290)
         y = random.randint(-290, 290)
         food.goto(x, y)
 
+        # add tail
+        new_seg = turtle.Turtle()
+        new_seg.speed(0)
+        new_seg.shape("square")
+        new_seg.color("white")
+        new_seg.penup()
+        seg.append(new_seg)
+
+        delay -= 0.001
+
+    # move the tail
+    for index in range(len(seg) - 1, 0, -1):
+        x = seg[index - 1].xcor()
+        y = seg[index - 1].ycor()
+        seg[index].goto(x, y)
+
+    if len(seg) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        seg[0].goto(x, y)
+
     move()
+
+    # Checks for collision with border
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        for segment in seg:
+            segment.goto(1000, 1000)
+
+        seg.clear()
+
+        delay = 0.1
+
+    # Checks for collision with body
+    for segment in seg:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            for segment in seg:
+                segment.goto(1000, 1000)
+
+            seg.clear()
 
     time.sleep(delay)
 
